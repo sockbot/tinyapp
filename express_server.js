7fea1c94e5d909b0cb3fe34d84e85a34872ea3f2
sockbot <github@sockbot.com>
2019-08-06 16:04:26 +0000

    added support for deleting URLs

--- express_server.js
+++ express_server.js
@@ -35,6 +35,11 @@ app.post('/urls', (req, res) => {
   res.redirect(`/urls/${randomStr}`)
 })
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls')
+})
+
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 })
